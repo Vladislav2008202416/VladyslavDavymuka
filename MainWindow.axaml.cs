afb2f4f689b6fefff9726276f8a6c78ef3902674
Владислав <vladislavdavimuka@MacBook-Pro-Vladislav.local>
2026-02-08 18:10:25 +0200

    Мій перший коміт

---
+++ MainWindow.axaml.cs
@@ -0,0 +1,24 @@
+using Avalonia.Controls;
+
+namespace MyFirstApp;
+// 2 мітки main dev github робити це все через git push
+public partial class MainWindow : Window
+{
+
+    public MainWindow()
+    {
+        InitializeComponent();
+    }
+    // Цей метод спрацює, коли натиснеш кнопку
+public void Button_Click(object sender, Avalonia.Interactivity.RoutedEventArgs e)
+{
+    // Знаходимо текстовий блок на ім'я "MyText" і змінюємо його
+    var textBlock = this.FindControl<TextBlock>("MyText");
+    if (textBlock != null)
+    {
+        textBlock.Text = "Ти натиснув кнопку!";
+    }
+
+
+}
+}
